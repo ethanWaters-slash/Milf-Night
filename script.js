386cb1603ad4c9656f2dccf19c5c22e4a8a8cbb9
ethanWaters-slash <76132706+ethanWaters-slash@users.noreply.github.com>
2023-07-10 11:57:01 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+// JavaScript for Carousel
+var slides = document.querySelectorAll(".mySlides");
+var i = 0;
+
+function plusSlides(n) {
+  i += n;
+  if (i > slides.length - 1) {
+    i = 0;
+  } else if (i < 0) {
+    i = slides.length - 1;
+  }
+  for (var slide of slides) {
+    slide.style.display = "none";
+  }
+  slides[i].style.display = "block";
+}
+
+// Call plusSlides to display the initial slide
+plusSlides(0);
+
+setInterval(function() {
+  plusSlides(1);
+}, 3500);
+
+const merchItems = document.querySelectorAll('.merch-item');
+
+// Add event listeners for mouseenter and mouseleave on each merch item
+merchItems.forEach((item) => {
+  const image = item.querySelector('img');
+  const soldOutMessage = document.createElement('div');
+  soldOutMessage.textContent = 'Sold Out';
+  soldOutMessage.classList.add('sold-out-message');
+
+  item.addEventListener('mouseenter', () => {
+    // Check if the item is sold out
+    const isSoldOut = item.classList.contains('sold-out');
+
+    if (isSoldOut) {
+      // Display "Sold Out" message
+      image.style.opacity = '0.5';
+      image.insertAdjacentElement('afterend', soldOutMessage);
+    }
+  });
+
+  item.addEventListener('mouseleave', () => {
+    // Remove the "Sold Out" message when mouse leaves
+    image.style.opacity = '1';
+    soldOutMessage.remove();
+  });
+});
+
+const tourDays = document.querySelectorAll('.tour-day');
+const selectedDateInfo = document.getElementById('selected-date-info');
+
+tourDays.forEach((day) => {
+  day.addEventListener('click', () => {
+    // Clear the selected class from all dates
+    tourDays.forEach((d) => {
+      d.classList.remove('selected');
+    });
+
+    // Add the selected class to the clicked date
+    day.classList.add('selected');
+
+    // Check if the selected date is available
+    const isAvailable = isDateAvailable(day.textContent);
+
+    // Extract the date and city from the text
+    const [date, city] = day.textContent.split(' - ');
+
+    // Update the selected date info
+    const selectedDate = `${day.parentNode.previousElementSibling.textContent} ${date} - ${city}, `;
+    if (isAvailable) {
+      selectedDateInfo.textContent = `Selected Date: ${selectedDate}`;
+      selectedDateInfo.style.color = '#000'; // Set the color for available date
+    } else {
+      selectedDateInfo.textContent = `Selected Date: ${selectedDate} (Sold Out)`;
+      selectedDateInfo.style.color = '#f00'; // Set the color for sold out date
+    }
+  });
+});
+
+
+// Function to check if a date is available
+function isDateAvailable(date) {
+  // Implement your logic to check if the date is available or sold out
+  // You can use an array, database, or any other data source to store the available dates
+  // Return true if the date is available, and false if it is sold out
+  // Example:
+  const availableDates = [];
+  return availableDates.includes(date);
+}
+
+// Play and Pause Controls
+var audio = document.querySelector('audio');
+var playButton = document.getElementById('play');
+var pauseButton = document.getElementById('pause');
+
+playButton.addEventListener('click', function() {
+  audio.play();
+});
+
+pauseButton.addEventListener('click', function() {
+  audio.pause();
+});
