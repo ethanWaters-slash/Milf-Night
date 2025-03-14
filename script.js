f81c063f71a1b39c0715e55771bbaa603e6e9fa5
ethanWaters-slash <76132706+ethanWaters-slash@users.noreply.github.com>
2025-03-14 14:03:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-// Check if the browser supports service workers
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-            .then(reg => console.log('Service Worker registered!', reg))
-            .catch(err => console.log('Service Worker registration failed:', err));
-    });
-}
+
 
 var slides = document.querySelectorAll(".mySlides");
 var i = 0;
